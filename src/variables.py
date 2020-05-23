df11853d0d73f72326214f40f3eefb0d93f20baf
Inf1delis <v.mamedov@g.nsu.ru>
2020-05-23 12:31:02 +0800

    added new user_id to USERS_ID_TO_SEND in variables.py

--- src/variables.py
+++ src/variables.py
@@ -15,7 +15,7 @@ TODAY_VOLUME_LOGFILE = '../logs/today_volume.log'
 VOLUME_GENERATOR_LOGFILE = '../logs/volume_generator.log'
 
 REDIRECTION_TO_TELEGRAM_LOGFILE = '../logs/redirection_to_telegram.log'
-USERS_ID_TO_SEND = [197079657]
+USERS_ID_TO_SEND = [197079657, 283754480]
 
 MONGO_TODAY_VOLUME_COLL = 'volume_today'
 MONGO_HISTORY_VOLUME_COLL = 'volume_history'
