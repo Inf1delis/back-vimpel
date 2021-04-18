30aa332b3bbaddd3088dce21287f6c731b11f6a7
Valentin Mamedov <45292985+Inf1delis@users.noreply.github.com>
2021-04-18 21:04:31 +0700

    Добавлена логика оформления заявки

--- src/routes/redirect_to_telegram.py
+++ src/routes/redirect_to_telegram.py
@@ -8,16 +8,37 @@ from telegram_bot.bot import bot
 log.configure_logging(variables.REDIRECTION_TO_TELEGRAM_LOGFILE)
 
 
+def handle_order(order_d):
+    table = []
+    goods = order_d['goods']
+    table += ['\t'.join([cols[k] for k in goods[0]])]
+    template = '\t'.join(["{"+k+"}" for k in goods[0]])
+
+    for d in goods:
+        table += [template.format(**d)]
+
+    table += ['']
+    table += ['Общая цена: ' + str(order_d['total_cost'])]
+
+    text_table = '\n'.join(table)
+    return text_table
+
 @cross_origin()
 def redirection_to_telegram():
     try:
         content = request.json
         if content is not None:
-            if content.get("name") is not None and \
-                    content.get("phone") is not None:
-                text = 'Имя: ' + content["name"] + '\nТелефон: ' + content["phone"] + '\n'
-                if content['desc']:
-                    text += 'Описание: ' + content["desc"]
+            text_a = []
+            if content.get("name"):
+                text_a += ['Имя: ' + content["name"]]
+            if content.get("phone"):
+                text_a += ['Телефон: ' + content["phone"]]
+            if content.get('desc'):
+                text_a += ['Описание: ' + content["desc"]]
+            if content.get('order'):
+                text_a += ['Заказ:\n' + handle_order(content["order"])]
+            if text_a:
+                text = '\n'.join(text_a)
                 for user_id in variables.USERS_ID_TO_SEND:
                     bot.send_message(chat_id=user_id, text=text)
         else:
